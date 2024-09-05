32d52d406c60accb148a002da64ad0fb3bdcca08
kibson6781 <121091967+kibson6781@users.noreply.github.com>
2024-09-06 01:49:28 +0530

    Create constants.php

---
+++ assets/includes/constants.php
@@ -0,0 +1,7 @@
+<?php
+// Database configuration
+$HOSTNAME = 'localhost';  // or your host
+$DB_USERNAME = 'root';     // or your database username
+$DB_PASSWORD = '';         // or your database password
+$DB_NAME = 'your_database';  // or your database name
+?>
